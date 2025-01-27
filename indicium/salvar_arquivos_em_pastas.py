7e192dbc58b3d2ea88f2e4526540ef8c6bd94a2b
Sophia <90727699+SophiaNCM@users.noreply.github.com>
2025-01-27 10:44:18 -0300

    Update salvar_arquivos_em_pastas.py

--- indicium/salvar_arquivos_em_pastas.py
+++ indicium/salvar_arquivos_em_pastas.py
@@ -4,8 +4,8 @@ import shutil
 
 path = '/home/sophia/DesafioIndicium/indicium/csvParaIncluir'
 data_atual = datetime.date.today()
-Newpath = '/home/sophia/DesafioIndicium/indicium/indicium_output'
+Newpath = '/home/sophia/DesafioIndicium/indicium/data/csv'
 files = os.listdir(path)
 for file in files:
     txtfile = file[:-4:]
-    os.rename(f'/home/sophia/DesafioIndicium/indicium/csvParaIncluir/{file}',f'{Newpath}\public-{file}')
+    os.rename(f'/home/sophia/DesafioIndicium/indicium/csvParaIncluir/{file}',f'{Newpath}/{txtfile}/'+data_atual.strftime('%Y-%m-%d')+f'/public-{file}')
